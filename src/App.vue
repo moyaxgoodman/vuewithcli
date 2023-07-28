4d0f1b0374158e0f811180c2c506af512f15f6f2
moyaxgoodman <jeremytelmoro200025@gmail.com>
2023-07-28 09:59:40 +0800

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome..</p>
+  <teleport to="#modals" v-if="showModal">
+    <modal theme="" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Ninja Giveaway!</h1>
+      <p>Grab yours now!</p>
+    </modal>
+  </teleport>
+  <!--<input type="text" ref="name">
+  <button @click="handleClick">click me</button>-->
+
+  <teleport to='#modals' v-if="showModalTwo">
+    <modal @close="toggleModalTwo">
+      <h1>Sign up to the newsletter</h1>
+      <p>For more updates and promo codes!</p>
+    </modal>
+  </teleport>
+
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModalTwo">Open Modal</button>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import modal from "./components/modal.vue"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { modal },
+  data() {
+    return {
+      title: 'My First Vue App',
+      header: 'Sign up for the Giveaway!',
+      text: 'Kasabot na gamay',
+      showModal: false,
+      showModalTwo: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    }
   }
+
+
+  //methods: {
+  // handleClick() {
+  // console.log(this.$refs.name)
+  // this.$refs.name.classList.add('active')
+  // this.$refs.name.focus()
+  //}
+
 }
 </script>
 
 <style>
-#app {
+#app,
+#modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
